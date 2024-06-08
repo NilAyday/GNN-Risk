ed117a9232dcd47a66c85acea8834753410f94ad
NilAyday <73837355+NilAyday@users.noreply.github.com>
2024-06-08 18:01:01 +0200

    Update read.py

--- read.py
+++ read.py
@@ -27,7 +27,7 @@ def get_highest_accuracy(results, model_name):
     return max_acc,std, best_combination
 
 def main():
-    results_dir = 'results'  # Directory where the results pickle files are stored
+    results_dir = '/mnt/data-test/results'  # Directory where the results pickle files are stored
     model_name = 'model_1'  # Specify the model name to search for
     
     results = read_pickle_files(results_dir)
