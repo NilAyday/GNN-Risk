7b0cf3dccfc132254e7d2057a257511baa5746d0
NilAyday <73837355+NilAyday@users.noreply.github.com>
2024-06-09 22:42:42 +0200

    Update model.py

--- model.py
+++ model.py
@@ -263,6 +263,11 @@ class my_GraphConvolution6(nn.Module):
         super(my_GraphConvolution6, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
+        print("before error")
+        print(nfeat)
+        print(n)
+        print(in_features)
+        print(in_features + n + nfeat)
         print(torch.FloatTensor(in_features + n + nfeat, out_features))
         self.weight = Parameter(torch.FloatTensor(in_features + n + nfeat, out_features))
         if bias:
