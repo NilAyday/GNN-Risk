fd49bf4f430ff1206daed8d8bd63d196abe5e2ae
NilAyday <73837355+NilAyday@users.noreply.github.com>
2024-06-27 12:26:59 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -132,3 +132,43 @@ def load_citation(dataset_str="cora"):
     adj = sys_normalized_adjacency(adj)
     adj = sparse_mx_to_torch_sparse_tensor(adj)
     return adj, features, labels, idx_train, idx_val, idx_test
+
+def load_data(dataset="cora"):
+    """Load citation network dataset."""
+    print('Loading {} dataset...'.format(dataset))
+
+    idx_feature_labels = np.genfromtxt("data/{}/{}.content".format(dataset, dataset), dtype=np.dtype(str))
+    feature = sp.csr_matrix(idx_feature_labels[:, 1:-1], dtype=np.float32)
+    labels = encode_onehot(idx_feature_labels[:, -1])
+
+    # build graph
+    idx = np.array(idx_feature_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("data/{}/{}.cites".format(dataset, dataset), dtype=np.int32)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+
+    # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    G = nx.read_edgelist("data/{}/{}.cites".format(dataset, dataset), nodetype=int, delimiter='\t')
+
+    print('Dataset has {} nodes, {} edges, {} features.'.format(adj.shape[0], edges.shape[0], feature.shape[1]))
+
+    n=adj.shape[0]
+    idx_train, temp_idx = sklearn.model_selection.train_test_split(np.arange(n), test_size=0.95, random_state=42)
+    idx_val, idx_test = sklearn.model_selection.train_test_split(temp_idx, test_size=0.9, random_state=42)
+
+    idx_train= torch.tensor(idx_train, dtype=torch.long)
+    idx_val = torch.tensor(idx_val, dtype=torch.long)
+    idx_test = torch.tensor(idx_test, dtype=torch.long)
+
+   
+    features = torch.from_numpy(feature.toarray())
+    adj = torch.from_numpy(adj.toarray())
+    labels=torch.argmax(torch.from_numpy(labels), dim=1)
+
+    return adj, features, labels, idx_train, idx_val, idx_test
+   
